b251a1f79f6ac7e77653c65f8cae601a1f139fef
LeonSalvo <leon.salvo@correo.ucu.edu.uy>
2023-06-26 17:53:07 -0300

    avance

--- Assets/Scripts/MovimientoBoss.cs
+++ Assets/Scripts/MovimientoBoss.cs
@@ -7,14 +7,17 @@ public class MovimientoBoss : MonoBehaviour
     [SerializeField] public double vida;
     [SerializeField] private LayerMask suelo;
     [SerializeField] public int daño;
-    [SerializeField] public double distanciaDaño;
     [SerializeField] public int dañoHabilidad1;
     [SerializeField] public double distanciaHabilidad1;
+    [SerializeField] public float rangoHabilidad1;
     [SerializeField] public int dañoHabilidad2;
     [SerializeField] public double distanciaHabilidad2;
+    [SerializeField] public float rangoHabilidad2;
     [SerializeField] public int dañoHabilidad3;
     [SerializeField] public double distanciaHabilidad3;
+    [SerializeField] public float rangoHabilidad3;
     [SerializeField] private float velocidad;
+    [SerializeField] LayerMask playerMask;
     private Rigidbody2D rb;
     private CircleCollider2D circleCollider;
     private GameObject player;
@@ -22,7 +25,7 @@ public class MovimientoBoss : MonoBehaviour
     private bool puedeSaltar;
     private bool estaActivo;
     private float direccion;
-
+    private bool estaAtacando;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +34,7 @@ public class MovimientoBoss : MonoBehaviour
         circleCollider = GetComponent<CircleCollider2D>();
         puedeSaltar = false;
         estaActivo = false;
+        estaAtacando = false;
     }
 
     // Update is called once per frame
@@ -44,8 +48,21 @@ public class MovimientoBoss : MonoBehaviour
                 direccion = 1;
             }
             rb.velocity = new Vector2(direccion * velocidad, rb.velocity.y);
-        } 
+        }else if (distanciaAlJugador < distanciaHabilidad3 && distanciaAlJugador > distanciaHabilidad2){
+            //mentita gira hasta chocar con una pared y cambia de direccion 2 o 3 veces al final rebota hacia sentido contrario
 
+        }else if (distanciaAlJugador < distanciaHabilidad3 && distanciaAlJugador > distanciaHabilidad2){
+            
+        }else if (distanciaAlJugador < distanciaHabilidad1){
+            //ataque en caida
+            slam();
+        }
+    }
+    private void slam(){
+        Collider2D[] hitEnemigos = Physics2D.OverlapCircleAll(transform.position, circleCollider.radius * 2, playerMask);
+        foreach(Collider2D player in hitEnemigos){
+            Debug.Log("aia");
+        }
     }
     public void setActivo(bool booleano){
         estaActivo = booleano;
